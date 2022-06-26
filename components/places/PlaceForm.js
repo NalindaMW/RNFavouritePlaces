2c76954badf7a5189104a65a9befd3716a88f985
Nalinda Wickramarathna <mastersilicon@gmail.com>
2022-06-26 23:33:09 +0530

    + Started designing Place Form component

--- components/places/PlaceForm.js
+++ components/places/PlaceForm.js
@@ -1,14 +1,47 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import { ScrollView, StyleSheet, Text, TextInput, View } from "react-native";
+import React, { useState } from "react";
+import { Colors } from "../../Constants/colors";
 
 const PlaceForm = () => {
+  const [enteredTitle, setEnteredTitle] = useState("");
+
+  function changeTitleHandler(enteredTitle) {
+    setEnteredTitle(enteredTitle);
+  }
+
   return (
-    <View>
-      <Text>Place Form</Text>
-    </View>
+    <ScrollView style={styles.form}>
+      <View>
+        <Text style={styles.label}>Title</Text>
+        <TextInput
+          onChangeText={changeTitleHandler}
+          value={enteredTitle}
+          style={styles.input}
+        />
+      </View>
+    </ScrollView>
   );
 };
 
 export default PlaceForm;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  form: {
+    flex: 1,
+    padding: 24,
+  },
+  label: {
+    fontWeight: "bold",
+    marginBottom: 8,
+    color: Colors.primary500,
+  },
+  input: {
+    marginVertical: 8,
+    paddingHorizontal: 4,
+    paddingVertical: 8,
+    fontSize: 16,
+    borderBottomWidth: 2,
+    borderBottomColor: Colors.primary700,
+    backgroundColor: Colors.primary100,
+  },
+});
