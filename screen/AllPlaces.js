ec5f15eb23774c11c40e4258830c0d28fbb2f7ce
Nalinda Wickramarathna <mastersilicon@gmail.com>
2022-07-02 21:36:26 +0530

    + Added fetch places from db

--- screen/AllPlaces.js
+++ screen/AllPlaces.js
@@ -2,6 +2,7 @@ import { StyleSheet } from "react-native";
 import React, { useEffect, useState } from "react";
 import PlacesList from "../components/places/PlacesList";
 import { useIsFocused } from "@react-navigation/native";
+import { fetchPlaces } from "../util/database";
 
 const AllPlaces = ({ route }) => {
   const [loadedPlaces, setLoadedPlaces] = useState([]);
@@ -9,13 +10,19 @@ const AllPlaces = ({ route }) => {
   const isFocused = useIsFocused();
 
   useEffect(() => {
-    if (isFocused && route.params) {
-      setLoadedPlaces((currentPlaces) => [
-        ...currentPlaces,
-        route.params.place,
-      ]);
+    async function loadPlaces() {
+      const places = await fetchPlaces();
+      setLoadedPlaces(places);
     }
-  }, [isFocused, route]);
+
+    if (isFocused) {
+      loadPlaces();
+      // setLoadedPlaces((currentPlaces) => [
+      //   ...currentPlaces,
+      //   route.params.place,
+      // ]);
+    }
+  }, [isFocused]);
 
   return <PlacesList places={loadedPlaces} />;
 };
